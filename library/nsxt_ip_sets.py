a24c9fa1df6a68db75cb5b407900cc02e448e728
Matt Proud <proudmatt@gmail.com>
2019-04-12 17:01:31 +0100

    Added modules. TODO examples.
    Signed-off-by: Matt Proud <proudmatt@gmail.com>

---
+++ library/nsxt_ip_sets.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 VMware, Inc.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+DOCUMENTATION = '''
+---
+module: nsxt_ip_sets
+short_description: 'Create an IP set'
+description: "Creates a new IPv4 or IPv6 address set. Required parameters are
+              allocation_ranges and cidr. Optional parameters are display_name,
+              description, dns_nameservers, dns_suffix, and gateway_ip."
+version_added: '2.7'
+author: 'Matt Proud'
+options:
+    hostname:
+        description: 'Deployed NSX manager hostname.'
+        required: true
+        type: str
+    username:
+        description: 'The username to authenticate with the NSX manager.'
+        required: true
+        type: str
+    password:
+        description: 'The password to authenticate with the NSX manager.'
+        required: true
+        type: str
+    display_name:
+        description: 'Display name'
+        required: true
+        type: str
+    state:
+        choices:
+            - present
+            - absent
+        description: "State can be either 'present' or 'absent'.
+                      'present' is used to create or update resource.
+                      'absent' is used to delete resource."
+        required: true
+    ip_addresses:
+        description: "ip_addresses can be IPv4 or IPv6 and they should not overlap. The maximum
+                      number will not exceed 5 ip_addresses."
+        required: false
+        type: 'list'
+    tags:
+        description: 'Opaque identifiers meaningful to the API user'
+        required: false
+        type: str
+
+    
+'''
+
+EXAMPLES = '''
+- name: Create ip set
+  nsxt_ip_sets:
+    hostname: "10.192.167.137"
+    username: "admin"
+    password: "Admin!23Admin"
+    validate_certs: False
+    display_name: IPset-IPV4-1
+    ip_addresses:
+    - "10.112.201.28"
+    - "10.112.201.64/26"
+    state: "present"
+'''
+
+RETURN = '''# '''
+
+
+import json, time
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.vmware_nsxt import vmware_argument_spec, request
+from ansible.module_utils._text import to_native
+
+def get_ip_set_params(args=None):
+    args_to_remove = ['state', 'username', 'password', 'port', 'hostname', 'validate_certs']
+    for key in args_to_remove:
+        args.pop(key, None)
+    for key, value in args.copy().items():
+        if value == None:
+            args.pop(key, None)
+    return args
+
+def get_ip_sets(module, manager_url, mgr_username, mgr_password, validate_certs):
+    try:
+      (rc, resp) = request(manager_url+ '/ip-sets', headers=dict(Accept='application/json'),
+                      url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+    except Exception as err:
+      module.fail_json(msg='Error accessing ip sets. Error [%s]' % (to_native(err)))
+    return resp
+
+def get_ip_set_from_display_name(module, manager_url, mgr_username, mgr_password, validate_certs, display_name):
+    ip_sets = get_ip_sets(module, manager_url, mgr_username, mgr_password, validate_certs)
+    for ip_set in ip_sets['results']:
+        if ip_set.__contains__('display_name') and ip_set['display_name'] == display_name:
+            return ip_set
+    return None
+
+# def ordered(obj):
+#     if isinstance(obj, dict):
+#         return sorted((k, ordered(v)) for k, v in obj.items())
+#     if isinstance(obj, list):
+#         return sorted(ordered(x) for x in obj)
+#     else:
+#         return obj
+
+def check_for_update(module, manager_url, mgr_username, mgr_password, validate_certs, ip_set_params):
+    existing_ip_set = get_ip_set_from_display_name(module, manager_url, mgr_username, mgr_password, validate_certs, ip_set_params['display_name'])
+    if existing_ip_set is None:
+        return False
+    if  existing_ip_set.__contains__('ip_addresses') and ip_set_params.__contains__('ip_addresses') and existing_ip_set['ip_addresses'] != ip_set_params['ip_addresses']:
+        return True
+    return False
+
+def main():
+  argument_spec = vmware_argument_spec()
+  argument_spec.update(display_name=dict(required=True, type='str'),
+                        description=dict(required=False, type='str'),
+                        ip_addresses=dict(required=False, type='list'),
+                        tags=dict(required=False, type='str'),
+                        state=dict(required=True, choices=['present', 'absent']))
+
+  module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
+  ip_set_params = get_ip_set_params(module.params.copy())
+  state = module.params['state']
+  mgr_hostname = module.params['hostname']
+  mgr_username = module.params['username']
+  mgr_password = module.params['password']
+  validate_certs = module.params['validate_certs']
+  display_name = module.params['display_name']
+  manager_url = 'https://{}/api/v1'.format(mgr_hostname)
+
+  set_dict = get_ip_set_from_display_name (module, manager_url, mgr_username, mgr_password, validate_certs, display_name)
+  set_id, revision = None, None
+  if set_dict:
+    set_id = set_dict['id']
+    revision = set_dict['_revision']
+
+  if state == 'present':
+    headers = dict(Accept="application/json")
+    headers['Content-Type'] = 'application/json'
+    updated = check_for_update(module, manager_url, mgr_username, mgr_password, validate_certs, ip_set_params)
+
+    if not updated:
+      # add the set
+      if module.check_mode:
+          module.exit_json(changed=True, debug_out=str(json.dumps(ip_set_params)), id='12345')
+      request_data = json.dumps(ip_set_params)
+      try:
+          if set_id:
+              module.exit_json(changed=False, id=set_id, message="IP set with display_name %s already exist."% module.params['display_name'])
+          (rc, resp) = request(manager_url+ '/ip-sets', data=request_data, headers=headers, method='POST',
+                                url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+      except Exception as err:
+          module.fail_json(msg="Failed to add ip set. Request body [%s]. Error[%s]." % (request_data, to_native(err)))
+
+      time.sleep(5)
+      module.exit_json(changed=True, id=resp["id"], body= str(resp), message="IP set with display name %s created." % module.params['display_name'])
+    else:
+      if module.check_mode:
+          module.exit_json(changed=True, debug_out=str(json.dumps(ip_set_params)), id=set_id)
+      ip_set_params['_revision']=revision # update current revision
+      request_data = json.dumps(ip_set_params)
+      id = set_id
+      try:
+          (rc, resp) = request(manager_url+ '/ip-sets/%s' % id, data=request_data, headers=headers, method='PUT',
+                                url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+      except Exception as err:
+          module.fail_json(msg="Failed to update ip set with id %s. Request body [%s]. Error[%s]." % (id, request_data, to_native(err)))
+      time.sleep(5)
+      module.exit_json(changed=True, id=resp["id"], body= str(resp), message="ip set with set id %s updated." % id)
+
+  elif state == 'absent':
+    # delete the array
+    id = set_id
+    if id is None:
+        module.exit_json(changed=False, msg='No ip set exist with display name %s' % display_name)
+    if module.check_mode:
+        module.exit_json(changed=True, debug_out=str(json.dumps(ip_set_params)), id=id)
+    try:
+        (rc, resp) = request(manager_url + "/ip-sets/%s" % id, method='DELETE',
+                              url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs)
+    except Exception as err:
+        module.fail_json(msg="Failed to delete ip set with id %s. Error[%s]." % (id, to_native(err)))
+
+    time.sleep(5)
+    module.exit_json(changed=True, object_name=id, message="ip set with set id %s deleted." % id)
+
+
+if __name__ == '__main__':
+    main()
---
+++ library/nsxt_ns_groups.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 VMware, Inc.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: nsxt_ns_groups
+short_description: Register and Install NSX Components on a Node
+description:  Creates an NS Group with either static or dynamic memeber.
+              
+              Reference the API guide for which params can be used with which operations.
+
+version_added: "2.7"
+author: Matt Proud
+options:
+    hostname:
+        description: Deployed NSX manager hostname.
+        required: true
+        type: str
+    username:
+        description: The username to authenticate with the NSX manager.
+        required: true
+        type: str
+    password:
+        description: The password to authenticate with the NSX manager.
+        required: true
+        type: str
+    members:
+        description: 'List of members. Must conform to NSGroupSimpleExpression schema.'
+        required: False
+        type: list
+
+        op: 
+            choices: ['EQUALS', 'CONTAINS', 'STARTSWITH', 'ENDSWITH', 'NOTEQUALS']
+            description: "Operator used to check value against resource type"
+            required: True
+            type: str
+        resource_type: 
+            choices: ['NSGroupSimpleExpression']
+            description: "Simple property which must be passed with all members"
+            required: True
+            type: str
+        target_property: 
+            description: "Object property used to identify. See API guide for details."
+            required: True
+            type: str
+        target_type: 
+            choices: ['NSGroup', 'IPSet', 'MACSet', 'LogicalSwitch', 'LogicalPort', 'VirtualMachine', 
+                      'DirectoryGroup', 'VirtualNetworkInterface', 'TransportNode']
+            description: "Type of target object which is supported"
+            required: True
+            type: str
+        value: 
+            description: "Value used to identify member. This can be the unique object ID, which is 'id'
+                          for most objects. Virtual machine uses 'external_id'.
+                          Module supports looking up object IDs by name, but target_property must still
+                          be left as ID."
+            required: True
+            type: str
+    membership_criteria:
+        description: 'List of membership criteria. Members must conform to NSGroupTagExpression or 
+                      NSGroupComplexExpression schema.'
+        required: False
+        type: list
+
+        ## NSGroupTagExpression required options
+        resource_type: 
+            choices: ['NSGroupTagExpression']
+            description: "Simple property which must be passed with all members"
+            required: True
+            type: str
+        target_type: 
+            description: "Object property used to identify. See API guide for details."
+            required: True
+            type: str
+        scope:
+            description: "Scope of objects to filter for"
+            required: True
+            type: str
+        tag:
+            description: "Tag used to filter against"
+            required: True
+            type: str
+        
+        ## NSGroupComplexExpression required options
+        resource_type: 
+            choices: ['NSGroupComplexExpression']
+            description: "Simple property which must be passed with all members"
+            required: True
+            type: str
+        expressions:
+            description: "Simple property which must be passed with all members"
+            required: True
+            type: list
+            resource_type: 
+                choices: ['NSGroupTagExpression']
+                description: "Simple property which must be passed with all members"
+                required: True
+                type: str
+            target_type: 
+                choices: ['LogicalSwitch', 'LogicalPort', 'VirtualMachine', ]
+                description: "Object property used to identify. See API guide for details."
+                required: True
+                type: str
+            scope:
+                description: "Scope of objects to filter for"
+                required: True
+                type: str
+            scope_op:
+                description: "Operator to apply to the tag. Defaults to EQUALS. See API guide for options."
+                required: False
+                type: str
+            tag:
+                description: "Tag used to filter against"
+                required: True
+                type: str
+            tag_op:
+                description: "Operator to apply to the tag. Defaults to EQUALS. See API guide for options."
+                required: False
+                type: str
+                
+    display_name:
+        description: Display name
+        required: true
+        type: str
+
+    resource_type:
+        choices:
+        - NSGroup
+        description: Specifies NSGroup as object type
+        required: true
+        type: str
+    state:
+        choices:
+        - present
+        - absent
+        description: "State can be either 'present' or 'absent'. 
+                      'present' is used to create or update resource. 
+                      'absent' is used to delete resource."
+        required: true
+'''
+
+EXAMPLES = '''
+- name: Add NS Group with membership criteria
+  nsxt_ns_group:
+    hostname: "10.192.167.137"
+    username: "admin"
+    password: "Admin!23Admin"
+    validate_certs: False
+    display_name: 'ns_with_criteria'
+    resource_type: NSGroup
+    membership_criteria:
+      - resource_type: NSGroupTagExpression
+        target_type: 'LogicalSwitch'
+        scope: 'S1'
+        tag: 'T1'
+      - resource_type: NSGroupComplexExpression
+        expressions:
+          - resource_type: NSGroupTagExpression
+            target_type: 'LogicalPort'
+            scope: 'S1'
+            tag: 'T1'
+          - resource_type: NSGroupTagExpression
+            target_type: 'LogicalPort'
+            scope: 'S2'
+            tag: 'T2'
+    state: "present"
+
+
+- name: Add NS Group with static members
+  nsxt_ns_group:
+    hostname: "10.192.167.137"
+    username: "admin"
+    password: "Admin!23Admin"
+    validate_certs: False
+    display_name: 'ns_with_criteria'
+    resource_type: NSGroup
+  members:
+    - resource_type: NSGroupSimpleExpression
+      target_property: id
+      op: EQUALS
+      target_type: IPSet
+      value: 'ips_test1'
+    - resource_type: NSGroupSimpleExpression
+      target_property: id
+      op: EQUALS
+      target_type: IPSet
+      value: 'ips_test2'
+    state: "present"
+'''
+
+RETURN = '''# '''
+
+import json, time
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.vmware_nsxt import vmware_argument_spec, request
+from ansible.module_utils._text import to_native
+
+try:
+    from __main__ import display
+except ImportError:
+    # pylint: disable=ungrouped-imports; this is the standard way how to import
+    # the default display object in Ansible action plugins.
+    from ansible.utils.display import Display
+    display = Display()
+
+def get_ns_group_params(args=None):
+    args_to_remove = ['state', 'username', 'password', 'port', 'hostname', 'validate_certs']
+    for key in args_to_remove:
+        args.pop(key, None)
+    for key, value in args.copy().items():
+        if value == None:
+            args.pop(key, None)
+    return args
+
+def get_ns_groups(module, manager_url, mgr_username, mgr_password, validate_certs):
+    try:
+      (rc, resp) = request(manager_url+ '/ns-groups', headers=dict(Accept='application/json'),
+                      url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+    except Exception as err:
+      module.fail_json(msg='Error accessing NS Group. Error [%s]' % (to_native(err)))
+    return resp
+
+def get_ns_group_from_display_name(module, manager_url, mgr_username, mgr_password, validate_certs, display_name):
+    ns_groups = get_ns_groups(module, manager_url, mgr_username, mgr_password, validate_certs)
+    for ns_group in ns_groups['results']:
+        if ns_group.__contains__('display_name') and ns_group['display_name'] == display_name:
+            return ns_group
+    return None
+
+def get_id_from_display_name(module, manager_url, mgr_username, mgr_password, validate_certs, endpoint, display_name, exit_if_not_found=True, id_notation='id'):
+    try:
+      (rc, resp) = request(manager_url+ endpoint, headers=dict(Accept='application/json'),
+                      url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+    except Exception as err:
+      module.fail_json(msg='Error accessing id for display name %s. Error [%s]' % (display_name, to_native(err)))
+
+    for result in resp['results']:
+        if result.__contains__('display_name') and result['display_name'] == display_name:
+            return result[id_notation]
+    if exit_if_not_found:
+        module.fail_json(msg='No id exist with display name %s' % display_name)
+
+def update_params_with_id (module, manager_url, mgr_username, mgr_password, validate_certs, ns_group_params ):
+    endpoint_lookup = {'NSGroup': '/ns-groups', 'IPSet': '/ip-sets', 'MACSet': '/mac-sets', 'LogicalSwitch': '/logical-switches', 
+                          'LogicalPort': '/logical-ports', 'VirtualMachine': '/fabric/virtual-machines', 'TransportNode': '/transport-nodes'}
+    if ns_group_params['members']:
+        for counter, member in enumerate(ns_group_params['members']):
+          if member['target_type'] in endpoint_lookup.keys() and (member['target_property'] == 'id' or member['target_property'] == 'external_id'):
+            value = ns_group_params['members'][counter].pop('value', None)
+            if member['target_property'] == 'id':
+              ns_group_params['members'][counter]['value'] = get_id_from_display_name (module, manager_url, mgr_username, mgr_password, validate_certs,
+                          endpoint_lookup[member['target_type']], value)
+            elif member['target_property'] == 'external_id':
+              ns_group_params['members'][counter]['value'] = get_id_from_display_name (module, manager_url, mgr_username, mgr_password, validate_certs,
+                          endpoint_lookup[member['target_type']], value, True, 'external_id')
+    return ns_group_params
+
+def check_for_update(module, manager_url, mgr_username, mgr_password, validate_certs, ns_group_params):
+    existing_ns_group = get_ns_group_from_display_name(module, manager_url, mgr_username, mgr_password, validate_certs, ns_group_params['display_name'])
+    if existing_ns_group is None:
+        return False
+    if existing_ns_group.__contains__('ip_addresses') and ns_group_params.__contains__('ip_addresses') and \
+        existing_ns_group['ip_addresses'] != ns_group_params['ip_addresses']:
+        return True
+    return False
+
+def main():
+  argument_spec = vmware_argument_spec()
+  argument_spec.update(display_name=dict(required=True, type='str'),
+                    host_credential=dict(required=False, type='dict',
+                        username=dict(required=False, type='str'),
+                        password=dict(required=False, type='str', no_log=True),
+                        thumbprint=dict(required=False, type='str', no_log=True)),
+                    members=dict(required=False, type='list', 
+                        op=dict(required=True, type='str', choices=['EQUALS', 'CONTAINS', 'STARTSWITH', 'ENDSWITH', 'NOTEQUALS']),
+                        resource_type=dict(required=True, type='str', choices=['NSGroupSimpleExpression']),
+                        target_property=dict(required=True, type='str'),
+                        target_type=dict(required=True, type='str', choices=['NSGroup', 'IPSet', 'MACSet', 'LogicalSwitch', 'LogicalPort', 
+                                                                              'VirtualMachine', 'DirectoryGroup', 'VirtualNetworkInterface', 'TransportNode']),
+                        value=dict(required=True, type='str', choices=['NSGroupSimpleExpression'])
+                            ),
+                    membership_criteria=dict(required=False, type='list'),
+
+                    resource_type=dict(required=True, type='str', choices=['NSGroup']),
+                    state=dict(required=True, choices=['present', 'absent']))
+
+  module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True,
+                         required_if=[['resource_type', 'HostNode', ['os_type']],
+                                      ['resource_type', 'EdgeNode', ['deployment_config']]])
+  
+  ns_group_params = get_ns_group_params(module.params.copy())
+  display.banner('B-Original Params = ' + str(ns_group_params))
+  module.log(msg='Original Params = ' + str(ns_group_params))  
+  
+  state = module.params['state']
+  mgr_hostname = module.params['hostname']
+  mgr_username = module.params['username']
+  mgr_password = module.params['password']
+  validate_certs = module.params['validate_certs']
+  display_name = module.params['display_name']
+  manager_url = 'https://{}/api/v1'.format(mgr_hostname)
+
+  if ns_group_params['members'] == ['']:
+    ns_group_params['members'] = []
+  if ns_group_params['membership_criteria'] == ['']:
+    ns_group_params['membership_criteria'] = []
+
+  node_dict = get_ns_group_from_display_name (module, manager_url, mgr_username, mgr_password, validate_certs, display_name)
+  node_id, revision = None, None
+  if node_dict:
+    node_id = node_dict['id']
+    revision = node_dict['_revision']
+
+  if state == 'present':
+    headers = dict(Accept="application/json")
+    headers['Content-Type'] = 'application/json'
+    body = update_params_with_id (module, manager_url, mgr_username, mgr_password, validate_certs, ns_group_params)
+    #module.fail_json(msg="Lazy  %s " % ns_group_params)
+    module.log(msg='Updated Params = ' + str(ns_group_params))
+    updated = check_for_update(module, manager_url, mgr_username, mgr_password, validate_certs, body)
+
+    if not updated:
+      request_data = json.dumps(ns_group_params)
+      if module.check_mode:
+          module.exit_json(changed=True, debug_out=str(request_data), id='12345')
+      try:
+          if node_id:
+              module.exit_json(changed=False, id=node_id, message="NS Group with display_name %s already exist."% module.params['display_name'])
+          (rc, resp) = request(manager_url+ '/ns-groups', data=request_data, headers=headers, method='POST',
+                                url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+      except Exception as err:
+                module.fail_json(msg="Failed to add node. Request body [%s]. Error[%s]." % (request_data, to_native(err)))
+
+      module.exit_json(changed=True, id=resp["id"], body= str(resp), message="NS Group with display name %s created succcessfully." % module.params['display_name'])
+    else:
+      if module.check_mode:
+          module.exit_json(changed=True, debug_out=str(json.dumps(ns_group_params)), id=id)
+
+      ns_group_params['_revision'] = revision # update current revision
+      request_data = json.dumps(ns_group_params)
+      id = node_id
+      try:
+          (rc, resp) = request(manager_url+ '/ns-groups/%s' % id, data=request_data, headers=headers, method='PUT',
+                                url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+      except Exception as err:
+          module.fail_json(msg="Failed to update node wit id %s. Request body [%s]. Error[%s]." % (id, request_data, to_native(err)))
+      module.exit_json(changed=True, id=resp["id"], body= str(resp), message="NS Group with node id %s updated." % id)
+
+  elif state == 'absent':
+    # delete the array
+    id = node_id
+    if id is None:
+        module.exit_json(changed=False, msg='No NS Group exist with display name %s' % display_name)
+    if module.check_mode:
+        module.exit_json(changed=True, debug_out=str(json.dumps(ns_group_params)), id=id)
+    try:
+        (rc, resp) = request(manager_url + "/ns-groups/%s" % id, method='DELETE',
+                              url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs)
+    except Exception as err:
+        module.fail_json(msg="Failed to delete NS Group with id %s. Error[%s]." % (id, to_native(err)))
+
+    module.exit_json(changed=True, id=id, message="NG Group with node id %s deleted." % id)
+
+
+if __name__ == '__main__':
+    main()
