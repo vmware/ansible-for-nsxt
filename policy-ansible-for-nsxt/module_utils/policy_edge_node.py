6d8cfc2224ec9c46ccead59576bd52745025fb66
Xiaopei Liu <lxiaopei@vmware.com>
2019-07-05 18:35:34 +0800

    Fix to create tier0 gateway
    Fix issue https://github.com/vmware/ansible-for-nsxt/issues/98
    
    Signed-off-by: XiaoPei Liu <lxiaopei@vmware.com>

--- policy-ansible-for-nsxt/module_utils/policy_edge_node.py
+++ policy-ansible-for-nsxt/module_utils/policy_edge_node.py
@@ -22,4 +22,4 @@ class PolicyEdgeNode(object):
     @staticmethod
     def get_resource_base_url(site_id, enforcementpoint_id, edge_cluster_id):
         return '/infra/sites/{}/enforcement-points/{}/edge-clusters/{}/edge-nodes'.format(
-+            site_id, enforcementpoint_id,edge_cluster_id)
+            site_id, enforcementpoint_id,edge_cluster_id)
