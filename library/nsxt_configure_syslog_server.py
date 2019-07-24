14255fcabebee5f9715a1d88eac141eae00caafd
Aleksey Nishev <anishev@vmware.com>
2019-07-24 20:05:31 +0300

    Changes to syslog_server and max_auth_failures_limit
    Signed-off-by: Aleksey Nishev <anishev@vmware.com>

--- library/nsxt_configure_syslog_server.py
+++ library/nsxt_configure_syslog_server.py
@@ -27,24 +27,21 @@ author: Aleksey Nishev
 '''
 
 EXAMPLES = '''
-- name: Configure Backup Server
-    nsxt_configure_backup_server:
-        backup_enabled: True
-        seconds_between_backups: 10000
-        backup_server: 10.10.10.10
-        backup_server_port: 22
-        backup_protocol_name: "sftp"
-        backup_ssh_fingerprint: "SHA256:w2NgXhG2Nm76q9PL/bXWKkLbDS31uMLYttUe9eajPa"
-        backup_username: "backup_usr"
-        backup_password: "VMware1!"
-        backup_directory_path: "/nsxt-backups"
-        backup_passphrase: "VMware1!VMware1!"
-        backup_inventory_summary_interval: 300
-        hostname: "10.1.1.1"
-        username: "admin"
-        password: "VMware1!"
-        validate_certs: True
-        backup_state: present
+    nsxt_configure_syslog_server:
+        syslog_server: "{{syslog_server}}"
+        syslog_server_port: "{{syslog_server_port}}"
+        syslog_protocol: "{{syslog_protocol}}"
+        syslog_level: "{{syslog_level}}"
+        exporter_name: "{{exporter_name}}"
+        facilities: "{{facilities}}"
+        msgids: "{{msgids}}"
+        hostname: "{{hostname}}"
+        username: "{{username}}"
+        password: "{{password}}"
+        validate_certs: "{{validate_certs}}"
+        syslog_state: absent
+    with_items:
+    - "{{syslog_servers}}"
 '''
 
 RETURN = '''# '''
@@ -60,11 +57,14 @@ def main():
     argument_spec = vmware_argument_spec()
     argument_spec.update(syslog_server=dict(required=True, type='str'),
                          syslog_server_port=dict(required=True, type='int'),
-                         syslog_protocol_name=dict(required=True, type='str'),
+                         syslog_protocol=dict(required=True, type='str'),
                          syslog_level=dict(required=True, type='str'),
+                         exporter_name=dict(required=True, type='str'),
+                         facilities=dict(required=True, type='list'),
+                         msgids=dict(required=True, type='list'),
                          hostname=dict(required=True, type='str'),
                          username=dict(required=True, type='str'),
-                         password=dict(required=True, type='str'),
+                         password=dict(required=True, type='str', no_log=True),
                          validate_certs=dict(required=True, type='bool'),
                          syslog_state=dict(required=True, type='str'))
 
@@ -77,33 +77,51 @@ def main():
     validate_certs = module.params['validate_certs']
     syslog_server = module.params['syslog_server']
     syslog_server_port = module.params['syslog_server_port']
-    syslog_protocol_name = module.params['syslog_protocol_name']
+    syslog_protocol = module.params['syslog_protocol']
     syslog_level = module.params['syslog_level']
+    exporter_name = module.params['exporter_name']
+    facilities = module.params['facilities']
+    msgids = module.params['msgids']
 
     manager_url = 'https://{}/api/v1'.format(mgr_hostname)
     headers = dict(Accept="application/json")
     headers['Content-Type'] = 'application/json'
 
-    request_data_dict = dict()
-    request_data = json.dumps(request_data_dict)
-
-    if state == 'absent':
+    if state == 'present':
         # Disable syslog server config
-        request_data_dict[""] = ""
-    elif state == 'present':
+        request_data_dict = dict()
+        request_data_dict["exporter_name"] = exporter_name
+        request_data_dict["facilities"] = facilities
+        request_data_dict["level"] = syslog_level
+        request_data_dict["msgids"] = msgids
+        request_data_dict["port"] = syslog_server_port
+        request_data_dict["protocol"] = syslog_protocol
+        request_data_dict["server"] = syslog_server
+        request_data = json.dumps(request_data_dict)
+        method = "POST"
+        exporter_name_url = ""
+    elif state == 'absent':
         # Add syslog server configuration
-        request_data_dict[""] = ""
+        method = "DELETE"
+        exporter_name_url = "/" + exporter_name
+        request_data = ""
 
-    request_data = json.dumps(request_data_dict)
-
+    url = manager_url + '/node/services/syslog/exporters' + exporter_name_url
     try:
-        (rc, resp) = request(manager_url + '/', data=request_data, headers=headers, method='PUT',
+        (rc, resp) = request(url, data=request_data, headers=headers, method=method,
                              url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
+        if rc == 201:
+            module.exit_json(changed=True, message="Successfully added syslog server configuration")
+        elif rc == 200:
+            module.exit_json(changed=True, message="Successfully removed syslog server configuration for exporter named: {}".format(exporter_name))
+        else:
+            module.fail_json(msg="Failed to add syslog server configuration. Response code {}".format(rc))
     except Exception as err:
-        module.fail_json(msg="Failed to add syslog configuration. Request body [%s]. Error[%s]." % (request_data, to_native(err)))
-
-    time.sleep(5)
-    module.exit_json(changed=True, result=resp, message="Configuration for syslog server %s created." % module.params['backup_server'])
+        message = "Specified syslog exporter '" + module.params['exporter_name'] + "' already exists."
+        if err[1]["error_message"] == message:
+            module.exit_json(changed=False, message=message)
+        else:
+            module.fail_json(msg="Failed to add syslog configuration. Request body {}. Error: {}".format(request_data, err))
 
 if __name__ == '__main__':
     main()
