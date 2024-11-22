0516aa9c1e2919fb6783ad5da0ecc75cd9759161
Shaunak Ghosh <shaunak.ghosh@broadcom.com>
2024-11-22 00:34:18 -0800

    Change in the logic for checking failure during upgrade reverse order
    Signed-off-by: Shaunak Ghosh <shaunak.ghosh@broadcom.com>

--- plugins/module_utils/upgrade_reverse_order.py
+++ plugins/module_utils/upgrade_reverse_order.py
@@ -15,7 +15,7 @@
 from __future__ import absolute_import, division, print_function
 import time
 from ansible_collections.vmware.ansible_for_nsxt.plugins.module_utils.vmware_nsxt import request
-from ansible_collections.vmware.ansible_for_nsxt.plugins.module_utils.common_utils import get_attribute_from_endpoint, get_upgrade_orchestrator_node
+from ansible_collections.vmware.ansible_for_nsxt.plugins.module_utils.common_utils import get_upgrade_orchestrator_node
 from ansible.module_utils._text import to_native
 
 
@@ -25,8 +25,11 @@ MP_UPGRADE_DONE = False
 
 def check_upgrade_status_at_start(module, manager_url, mgr_username, mgr_password, validate_certs):
    global MP_UPGRADE_DONE
+
+   endpoint = "/upgrade/upgrade-unit-groups?sync=true"
+   call_get_sync(manager_url, endpoint, mgr_username, mgr_password, validate_certs)
    
-   upgrade_status_summary = get_upgrade_status_summary(module, manager_url, mgr_username, mgr_password, validate_certs)
+   upgrade_status_summary = get_upgrade_status_summary(module, manager_url, mgr_username, mgr_password, validate_certs , False)
 
    overall_upgrade_status = upgrade_status_summary['overall_upgrade_status']
 
@@ -36,37 +39,68 @@ def check_upgrade_status_at_start(module, manager_url, mgr_username, mgr_passwor
   
    return overall_upgrade_status
 
-def get_upgrade_status_summary(module, manager_url, mgr_username, mgr_password, validate_certs):
+def get_upgrade_status_summary(module, manager_url, mgr_username, mgr_password, validate_certs , ignore_errors):
   '''
   Get the upgrade status summary
   '''
-  endpoint = "/upgrade/upgrade-unit-groups?sync=true"
-  call_get_sync(manager_url, endpoint, mgr_username, mgr_password, validate_certs)
 
   try:
         (rc, resp) = request(manager_url+ UPGRADE_STATUS_SUMMARY_API, headers=dict(Accept='application/json'),
                       url_username=mgr_username, url_password=mgr_password, 
-                      validate_certs=validate_certs, ignore_errors=False)
+                      validate_certs=validate_certs, ignore_errors=ignore_errors)
   except Exception as err:
-          module.fail_json(msg='Error while triggering api:'
+          if ignore_errors:
+             return None
+          else:
+            module.fail_json(msg='Error while triggering api:'
                             ' %s. Error [%s]' % (manager_url+ UPGRADE_STATUS_SUMMARY_API, to_native(err)))
   return resp
 
 def call_get_sync(managerUrl, endpoint, mgrUsername, mgrPassword, validateCerts):
     request(managerUrl + endpoint, method='GET', url_username=mgrUsername, url_password=mgrPassword,
             validate_certs=validateCerts, ignore_errors=True)
-    
-def get_upgrade_status(module, manager_url, mgr_username, mgr_password, validate_certs):
-  '''
-  Get the current status of upgrade at the start.
-  Doesn't upgrade if any component is in progress 
-  or system is already upgraded.
-  '''
-  upgrade_status = get_attribute_from_endpoint(module, manager_url, '/upgrade/status-summary',
-                    mgr_username, mgr_password, validate_certs, 'overall_upgrade_status', 
-                    False)
-  
-  return upgrade_status
+
+def check_component_upgrade_completion_status(module, manager_url, mgr_username, mgr_password, validate_certs):
+   global MP_UPGRADE_DONE
+   count_upgrade_status_api_no_resp = 0
+   component_upgrade_start_time = time.time()
+
+   while True:
+      upgrade_status_summary = get_upgrade_status_summary(module, manager_url, mgr_username, mgr_password, validate_certs , True)
+
+      if upgrade_status_summary == None:
+        count_upgrade_status_api_no_resp +=1
+
+      elif upgrade_status_summary.__contains__('overall_upgrade_status'):
+         overall_upgrade_status = upgrade_status_summary['overall_upgrade_status']
+
+         if overall_upgrade_status == 'SUCCESS':
+            module.exit_json(changed=True, message='System has been upgraded successfully!!!')
+            
+         elif overall_upgrade_status == 'PAUSED':
+            check_component_statuses(module , upgrade_status_summary['component_status'])
+            if not MP_UPGRADE_DONE:
+                MP_UPGRADE_DONE = True
+            return
+        
+      elapsed_time = time.time - component_upgrade_start_time
+         
+      if MP_UPGRADE_DONE and count_upgrade_status_api_no_resp >= 5:
+        module.fail_json(msg='Error while triggering api:'
+                    ' %s. API failed 5 times' %UPGRADE_STATUS_SUMMARY_API)
+      elif not MP_UPGRADE_DONE and elapsed_time > 3600 :
+         module.fail_json(msg='MP component upgrade took longer than 1hr, System upgrade failed')
+
+      time.sleep(30)
+       
+
+def check_component_statuses(module, component_status_list):
+    for component_status in enumerate(component_status_list):
+        if component_status['status'] == 'FAILED':
+            module.fail_json(msg='Failed to upgrade system as Component : %s'
+                        'has Status : %s ' %(component_status['component_type'], component_status['status']))
+      
+   
 
 def execute_upgrade(module, manager_url, mgr_username, mgr_password, validate_certs):
    global MP_UPGRADE_DONE
@@ -81,29 +115,11 @@ def execute_upgrade(module, manager_url, mgr_username, mgr_password, validate_ce
                         validate_certs=validate_certs, ignore_errors=False)
       except Exception as err:
         module.fail_json(msg="Failed while upgrading component")
+
       time.sleep(30)
 
-      upgrade_status = ''
-      count_upgrade_status_api_no_resp = 0
+      check_component_upgrade_completion_status(module, manager_url, mgr_username, mgr_password, validate_certs)
 
-      while True:     
-         upgrade_status = get_upgrade_status(module, manager_url, mgr_username,
-                                          mgr_password, validate_certs)
-
-         if upgrade_status == 'SUCCESS' or upgrade_status == 'FAILED' or upgrade_status == 'PAUSED':
-            if not MP_UPGRADE_DONE:
-               MP_UPGRADE_DONE = True
-            break
-         elif upgrade_status == None :
-            count_upgrade_status_api_no_resp += 1
-            if MP_UPGRADE_DONE and count_upgrade_status_api_no_resp >= 5:
-               module.fail_json(msg='Error while triggering api:'
-                            ' %s. API failed 5 times' %UPGRADE_STATUS_SUMMARY_API)
-                 
-         time.sleep(30)
-         
-      if upgrade_status == 'SUCCESS' or upgrade_status == 'FAILED':
-         return upgrade_status
       
 
 def trigger_upgrade_reverse_order(module, mgr_hostname, mgr_username, mgr_password, validate_certs):
@@ -127,9 +143,4 @@ def trigger_upgrade_reverse_order(module, mgr_hostname, mgr_username, mgr_passwo
                     ' continue.')
       
     elif upgrade_status == 'NOT_STARTED' or upgrade_status == 'PAUSED':
-       upgrade_result = execute_upgrade(module, manager_url, mgr_username, mgr_password, validate_certs)
-
-       if upgrade_result == 'FAILED':
-           module.fail_json(msg='Failed to upgrade system')
-          
-    module.exit_json(changed=True, message='System has been upgraded successfully!!!')
+      execute_upgrade(module, manager_url, mgr_username, mgr_password, validate_certs)
